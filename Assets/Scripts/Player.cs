7e484129be5accfa1c1ecbecaf9b0b601eaca438
AlmaCoolCode <alma@coolcode.io>
2025-01-28 18:31:35 +0100

    Playercontrols

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rigidbody;
+    [SerializeField] float jumpspeed = 10;
+    [SerializeField] float movespeed = 10;
+    [SerializeField] int playerNumber = 1;
+    [SerializeField] Transform groundCheck;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,19 +19,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        float ySpeed = rigidbody.velocity.y;
+        if (Input.GetButton("Jump" + playerNumber&)
         {
-            rigidbody.AddForce(new Vector2(10, 0));
+            ySpeed = jumpspeed;
         }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            rigidbody.AddForce(new Vector2(-10, 0));
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody.AddForce(new Vector2(0, 100));
-        }
+        rigidbody.velocity = new Vector2(movespeed * Input.GetAxis("Horizontal" + playerNumber), ySpeed);
+    }
+    private bool IsOnGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.1f);
+        return hit.collider != null;
     }
 }
